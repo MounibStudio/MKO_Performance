af0dd8df59c931a40f6adb4278c368c9f3e2952d
MounibStudio <mounib.afailal16@gmail.com>
2026-05-08 16:23:21 +0100

    .env email

--- backend/settings.py
+++ backend/settings.py
@@ -118,8 +118,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'mkoperformance@gmail.com'
-EMAIL_HOST_PASSWORD = 'ndie pmdg mmwt svgq'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD  = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 DEFAULT_FROM_EMAIL = "MKO Performance <mkoperformance@gmail.com>"
 
